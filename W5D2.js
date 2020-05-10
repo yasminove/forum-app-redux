981f5fabb84a0cb7d8fb935dde550157d5597d11
Kareem Abdelwahed <kareem.abdelwahed.a@gmail.com>
2020-05-10 12:18:29 +0300

    Revert "try again"

--- W5D2.js
+++ W5D2.js
@@ -2,18 +2,10 @@
 1) implement the last version of each 
 */
 
-
-   function each(coll, f) {
-      if (Array.isArray(coll)) {
-      for (var i = 0; i < coll.length; i++) {
-      f(coll[i], i);
-    }
-  } else {
-    for (var key in coll) {
-      f(coll[key], key);
-    }
-  }
-}	
+function each(coll, func) {
+ //write your code here  
+}
+	
 
 /*
 1) implement map function
@@ -21,12 +13,9 @@
 */
 	function map(array, f) {
 	  var acc = [];
-	  each(array,function(element,i){
-	  	acc.push(f(element,i))
-	  })
+	  
 	  return acc;
 	}
-
 
 /*
 1) implement the improved map 
@@ -35,13 +24,6 @@
 
 function map(coll, f) {
 	var acc = [];
-      if (!Array.isArray(coll)) {
-	acc = {};
-  }
-  each(coll, function(element, key) {
-    acc[key] = f(element, key) ;
-  });
 	  
 	 return acc;
 }
-  ***
