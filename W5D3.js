b5f259f539292146b4df2353fc35420b8e21b5be
Kareem-Abdelwahed <kareem.abdelwahed.a@gmail.com>
2020-05-11 09:13:31 +0300

    W5D3.js

---
+++ W5D3.js
@@ -0,0 +1,16 @@
+/*
+1- Using map, Write a function called uppercaseValues that, given an object as a parameter,
+ returns a new object with all of its string values converted to uppercase. 
+Also, ensure that you only attempt to convert strings to uppercase.
+*/
+
+
+
+
+/*
+2- Using reduce, write a function that sums the squared values of an array of numbers and returns the sum result.
+*/
+function sum(numbers) {
+	//your code is here 
+
+}
