c7ff869d3096f956df803505f289bd26156ab9c9
hend_isleem <hendisleem@gmail.com>
2020-05-14 09:18:08 +0200

    W5D6

---
+++ W5D6.js
@@ -0,0 +1,17 @@
+/*
+
+1-Write a function called select that, given an object and an array of strings representing keys,
+returns a new object consisting of just the keys (and values) specified in the array of strings.
+
+function select(object, keys) { 
+       // TODO: your code here 
+} 
+select({a: 1, b: 2, c: 3}, ['a', 'b']); // => {a: 1, b: 2}
+*/
+
+
+
+/*
+2-write a function called sortAscending that takes an object, and returns array of sorted object values
+var ex = {a: 4, b: 10, c: 7, d: 2} // ==> outputs: [{d: 2}, {a: 4}, {c: 7}, {b: 10}]
+/*
