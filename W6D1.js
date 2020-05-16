ae3f9be0286c54505acae04c5a5f2bd54aeb3910
Kareem-Abdelwahed <kareem.abdelwahed.a@gmail.com>
2020-05-16 09:16:07 +0300

    W6D1

---
+++ W6D1.js
@@ -0,0 +1,25 @@
+// 0. create a new folder inside your warmUps folder and name it W6D1 
+// Inside this folder do the following:
+// 1. create new HTML file a with a three buttons and div with heading and paragraph inside it.
+//      a. button1 : Important
+//      b. button2 : normal
+//      c. button3 : rest
+// 2. create a new JS file and link between the js and HTML
+// 3. create a new css file and change the styles of the buttons and div with it's content
+// 4. add Interaction for the buttons as the following: 
+// when the Important button clicked! change the background color to red and text to white and heading to dark red
+// when the normal button clicked! change the background color to lightblue and text to white and heading to dark blue
+// when the rest button clicked! change the background white and the text to dark
+
+
+
+// you can use this code for the div content: 
+// Heading for the div
+// I'm a simple html paragraph and the css can controll my attributes. The JS will make the page interactive
+
+
+/*
+What is your expectations for this week?
+
+
+*/
