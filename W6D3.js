0e2a509738f1595fff4174d64067609732e1d5f9
Kareem-Abdelwahed <kareem.abdelwahed.a@gmail.com>
2020-05-18 09:14:03 +0300

    W6D3

---
+++ W6D3.js
@@ -0,0 +1,10 @@
+// 0. create a new folder inside your warmUps folder and name it W6D3
+// Inside this folder do the following:
+// Create an HTML, CSS, JS files and connect them together.
+// Create a text field and a button below it in HTML file
+// Create an empty unordered list  below the button
+// In CSS file, create two CSS classes, each with a different color property
+// Using jQuery, create a function, that takes the input from the text field, creates a list item and add it to the unordered list, 
+// then add a class to the added item alternatively (So first time Class1 then next time Class2 then Class1 and repeat)
+
+// Good Luck!
