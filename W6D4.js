6a06634057a25cb1bcdfbe28a1f2b93750dea4b6
hend_isleem <hendisleem@gmail.com>
2020-05-20 09:02:13 +0200

    W6D4

---
+++ W6D4.js
@@ -0,0 +1,9 @@
+// 0. create a new folder inside your warmUps folder and name it W6D3
+// Inside this folder do the following:
+// Create an HTML, CSS and JS files and connect them together
+// Create two buttons one that says “Left” and one that say “Right”
+// Create a button below them that says “Create”
+// Create two unordered lists next to each other
+// Write a jQuery function that creates a new element in the left unordered list if the “left” button was pressed before the “create” button
+// and to the right unordered list if the “right” button was pressed before the “create” button
+// Style the two unordered lists in CSS
